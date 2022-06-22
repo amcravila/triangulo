cced596d4f8bec7448c9a0c0d0d3f6ca36785ec2
amcravila <amcravila@gmail.com>
2022-06-22 19:50:48 -0300

    criar triangulo

---
+++ script.js
@@ -0,0 +1,41 @@
+const result = document.querySelector("#result")
+const botaoClick = document.querySelector("#botaoClick")
+
+const createTriangule = () => {
+  // let input1 = document.querySelector("#stars-number").value
+  // let input2 = document.querySelector("#stars-number2").value
+  // console.log(input1)
+  // result.innerHTML = input1 + input2  
+// document.querySelector('h1').innerHTML = `Novo título <h2>Texto H2</h2>`
+
+  result.innerHTML = ''
+  const starsNumber = document.querySelector("#stars-number").value
+  const star = '*'
+  // let arrayNovo = [10, 20, 30, 40]
+
+  // arrayNovo.map((elemento) => {
+  //   return result.innerHTML += `<div>${elemento}</div>`
+  // })
+  for(let i=1; i <= starsNumber; i++) {
+    result.innerHTML += `${star.repeat(i)} </br>`
+    // console.log(star.repeat(i), '\n')
+  }
+
+
+} 
+
+
+
+//   let arrayNovo = [10, 20, 30, 40]
+// let arrayModificado2  = []
+// const arrayModificado3 = arrayNovo.map((elemento) => {
+//   return elemento + 10
+// })
+
+
+
+// const novosNumeros = arrayNovo.map((elemento) => {
+//   return console.log(elemento + 10)
+// })
+
+// [20, 30, 40, 50]
